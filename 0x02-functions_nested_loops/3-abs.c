be0cdf66a80b76a33f236cb114652f43e1542f8f
mospakles <motunrayodusina@gmail.com>
2022-04-13 10:09:44 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/3-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_abs - Computes the absolute value of an integer.
+ *@n: The integer to be computed.
+ *Return: The absolute value of the integer
+ */
+
+int _abs(int n)
+{
+
+return (n>= 0 ? n: n * -1);
+
+}
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,31 +1,27 @@
 #include "main.h"
 
 /**
- *print_sign - Prints the sign of a number.
- *@n: The number of which the sign will be printed.
- *Return: 1 if the number is greater than zero,
- *0 if the number is zero,
- *-1 if the number is less than zero.
+ *print_sign - return 0 letter not lowercase, 1 letter lowercase
+ *@n: the int to print
+ *Return: Always 0.
  */
-
 int print_sign(int n)
 {
-int test;
 if (n > 0)
 {
-test = 1;
-_putchar('+')
+_putchar ('+');
+return (1);
+}
 
-}
-	else if (n == 0)
+else if (n == 0)
 {
-test = 0;
-_putchar('0');
+_putchar ('0');
+return (0);
 }
+
 else
 {
-test = -1;
-_putchar('-');
+_putchar ('-');
+return (-1);
 }
-		return (test);
 }
