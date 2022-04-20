6f8fc21519ea13d1ba422d1a7740f9b87f75c2ec
mospakles <motunrayodusina@gmail.com>
2022-04-20 15:22:42 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,27 +9,12 @@
  *Return: Always 0 (Success)
 */
 
-int main(void)
+nt main(void)
 {
-int i, sum, n;
-int pass[100];
+int num;
 
-sum = 0;
-srand(time(NULL));
-
-for (i = 0; i < 100; i++)
-{
-pass[i] = rand() % 78;
-sum += (pas[i] + '0');
-putchar (pass[i] + '0');
-if ((2772 - sum) -'0' < 78)
-{
-n = 2772 - sum - '0';
-sum += n;
-putchar (n + '0');
-break;
-}
-}
+srand(time(0));
+num = rand();
+printf("%i\n", num);
 return (0);
 }
-
