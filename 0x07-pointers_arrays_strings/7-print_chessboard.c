a97a70c58a39f18933666495db18b65bc2b83177
mospakles <motunrayodusina@gmail.com>
2022-04-26 14:56:13 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,21 +8,19 @@
 
 void print_chessboard(char (*a)[8])
 {
-/*Declaring variables*/
 int i, j;
 
-int i = 0;
-while (i < 8) /*number repetitions*/
+for (i = 0; i < 8; i++)
+
 {
 
-j = 0;
-while (j < 8)
+for (j = 0; j < 8; j++)
+
 {
 _putchar (a[i][j]);
-j++;
+
 }
 _putchar ('\n'); /*print new line*/
-i++; /*add +1*/
 }
 
 }
